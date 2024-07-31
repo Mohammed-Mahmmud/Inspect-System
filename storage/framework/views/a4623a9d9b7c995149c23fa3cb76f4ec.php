3a9a3e55d41bd740f16ef891e81c7fbade490560
Mohamed Khater <mhmedkhaater@gmail.com>
2024-07-31 12:21:41 +0300

    update

---
+++ storage/framework/views/a4623a9d9b7c995149c23fa3cb76f4ec.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>STC - login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css"
+        integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo e(asset('website/assets/layouts/login/style.css')); ?>">
+</head>
+<!--Coded with love by Mutiullah Samim-->
+
+<body>
+    <div class="container h-100">
+        <div class="d-flex justify-content-center h-100">
+            <div class="user_card">
+                <div class="d-flex justify-content-center">
+                    <div class="brand_logo_container">
+                        <img src="<?php echo e(asset('dashboard/images/background/stc_logo_old.png')); ?>" class="brand_logo"
+                            alt="Logo">
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center form_container">
+                    <form action="<?php echo e(route('frontend.login')); ?>" method="POST">
+                        <?php echo csrf_field(); ?>
+                        <div class="input-group mb-3">
+                            <?php if (isset($component)) { $__componentOriginal3ee8e6cd618674982f34cd16ebf8436b = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal3ee8e6cd618674982f34cd16ebf8436b = $attributes; } ?>
+<?php $component = App\View\Components\Dashboard\Layouts\ErrorVerify::resolve([] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? (array) $attributes->getIterator() : [])); ?>
+<?php $component->withName('dashboard.layouts.error-verify'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag && $constructor = (new ReflectionClass(App\View\Components\Dashboard\Layouts\ErrorVerify::class))->getConstructor()): ?>
+<?php $attributes = $attributes->except(collect($constructor->getParameters())->map->getName()->all()); ?>
+<?php endif; ?>
+<?php $component->withAttributes(['errors' => ''.e($errors).'']); ?> <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal3ee8e6cd618674982f34cd16ebf8436b)): ?>
+<?php $attributes = $__attributesOriginal3ee8e6cd618674982f34cd16ebf8436b; ?>
+<?php unset($__attributesOriginal3ee8e6cd618674982f34cd16ebf8436b); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal3ee8e6cd618674982f34cd16ebf8436b)): ?>
+<?php $component = $__componentOriginal3ee8e6cd618674982f34cd16ebf8436b; ?>
+<?php unset($__componentOriginal3ee8e6cd618674982f34cd16ebf8436b); ?>
+<?php endif; ?>
+                                <div class="input-group-append">
+                                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                </div>
+                                <input type="email" name="email" class="form-control input_user"
+                                    placeholder="email">
+                        </div>
+                        <div class="input-group mb-2">
+                            <div class="input-group-append">
+                                <span class="input-group-text"><i class="fas fa-key"></i></span>
+                            </div>
+                            <input type="password" name="password" class="form-control input_pass"
+                                placeholder="password">
+                        </div>
+                        <div class="form-group">
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" id="remember" name="remember">
+                                <label class="custom-control-label" for="remember">Remember me</label>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-center mt-3 login_container">
+                            <button type="submit" class="btn login_btn">Login</button>
+                        </div>
+                    </form>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+<?php /**PATH /home/mohamed-khater/Documents/projects/stc/inspectionSystem/Inspect-System/resources/views/website/auth/login.blade.php ENDPATH**/ ?>
